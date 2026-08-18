b2486e68fe5375174b15000fa21179cf8572d78b
henmorei <henmorei06@gmail.com>
2026-08-17 22:06:28 -0300

    Adição dos exercicios 3, 4 e 5.

--- 1-semestre/logica-programacao/aula02/exercicios.js
+++ 1-semestre/logica-programacao/aula02/exercicios.js
@@ -1,26 +1,8 @@
-// Elaborar os seguintes programas de entrada, processamento e saída.
-// - Elaborar um programa que leia 2 notas de um aluno. Calcule e mostre a média
-// das notas.
-
-// -Elaborar um programa que leia o valor de um jantar. Sabendo que a taxa do
-// garçom é de 10%, calcule e mostre a taxa do garçom e o valor total a ser pago.
-
-// -Elaborar um programa que leia o valor de um veículo. Mostre a promoção de
-// financiamento da revenda, que consiste em 50% de entrada e o valor do saldo
-// em 12x.
-
-// -Elaborar um programa que leia nome e idade de um aluno. Calcule o ano que
-// ele nasceu. Exiba mensagem informando o nome do aluno e seu ano de
-// nascimento.
-
-// -Elaborar um programa que leia um número. Exiba os vizinhos deste número
-// (anterior e posterior)
-
 const prompt = require("prompt-sync")();
 console.clear();
 
-function converteNum (num){
-  return Number(num.replace(',','.'));
+function converteNum(num) {
+  return Number(num.replace(',', '.'));
 }
 
 //////////////////////////////////////////////////////////////////
@@ -28,8 +10,11 @@ function converteNum (num){
 console.log("##### EXERCÍCIOS AULA 02 #####\n");
 console.log("(1) Cálculo de médias");
 console.log("(2) Cálculo de taxa do garçom");
+console.log("(3) Cálculo de financiamento");
+console.log("(4) Cálculo de idade");
+console.log("(5) Números vizinhos");
 
-const userSelect = Number(prompt("\nSelecione o execicio: "));
+const userSelect = Number(prompt("Selecione o execicio: "));
 switch (userSelect) {
   case 1:
     exercicio001();
@@ -37,9 +22,18 @@ switch (userSelect) {
   case 2:
     exercicio002();
     break
+  case 3:
+    exercicio003();
+    break
+  case 4:
+    exercicio004();
+    break
+  case 5:
+    exercicio005();
+    break
   default:
     prompt('Opção inválida, tente novamente....');
-  }
+}
 
 //////////////////////////////////////////////////////////////////
 
@@ -49,7 +43,7 @@ function exercicio001() {
   // das notas.
 
   console.clear();
-  console.log("\n#### Cálculo de médias ####\n");
+  console.log("#### Cálculo de médias ####\n");
   const quantiNotas = prompt("Quantas notas serão computadas? ");
   let arrayNotas = [];
   let somaNotas = 0;
@@ -66,8 +60,9 @@ function exercicio002() {
 
   // -Elaborar um programa que leia o valor de um jantar. Sabendo que a taxa do
   // garçom é de 10%, calcule e mostre a taxa do garçom e o valor total a ser pago.
+
   console.clear();
-  console.log('\n#### Cálculo de taxa do garçom ####\n');
+  console.log('#### Cálculo de taxa do garçom ####\n');
   valorTaxa = Number(prompt('Qual a porcentagem da taxa de serviço? '));
   valorJantar = prompt('Qual o valor  do jantar? ');
   valorJantar = converteNum(valorJantar);
@@ -76,7 +71,7 @@ function exercicio002() {
 
   console.log(`
     ================================================
-                     CUPOM FISCAL
+    CUPOM FISCAL
     ================================================
     Valor do jantar                R$${valorJantar.toFixed(2)}
     Porcentagem de serviço         ${valorTaxa}%
@@ -86,8 +81,59 @@ function exercicio002() {
     ================================================`);
 };
 
-function exercicio003(){
+function exercicio003() {
+
   // -Elaborar um programa que leia o valor de um veículo. Mostre a promoção de
   // financiamento da revenda, que consiste em 50% de entrada e o valor do saldo
   // em 12x.
-}
+
+  console.clear();
+  console.log('#### Cálculo de financiamento ####\n');
+
+  const valorCarro = Number(prompt('Qual o valor do veículo? '));
+  console.log(`
+    Valor de revenda
+
+    Valor de entrada R$${(valorCarro * 0.5).toFixed(2)}
+    + 12x R$${((valorCarro * 0.5) / 12).toFixed(2)}
+    `)
+
+};
+
+
+function exercicio004() {
+
+  // -Elaborar um programa que leia nome e idade de um aluno. Calcule o ano que
+  // ele nasceu. Exiba mensagem informando o nome do aluno e seu ano de
+  // nascimento.
+
+  console.clear();
+  console.log('#### Cálculo de idade ####\n');
+
+  const entradaNome = prompt("Digite seu nome: ");
+  const entradaData = prompt("Digite sua data de nascimento (DD/MM/YYYY): ");
+
+  const partes = entradaData.split('/');
+
+  const diaNasc = parseInt(partes[0], 10);
+  const mesNasc = parseInt(partes[1], 10);
+  const anoNasc = parseInt(partes[2], 10);
+
+  const hoje = new Date();
+  let idade = hoje.getFullYear() - anoNasc;
+  const mesAtual = hoje.getMonth() + 1;
+  const diaAtual = hoje.getDate();
+
+  if (mesAtual < mesNasc || (mesAtual === mesNasc && diaAtual < diaNasc)) {
+    idade--;
+  }
+
+  console.log(`${entradaNome}, sua idade é ${idade} anos.`);
+};
+
+function exercicio005() {
+  console.log('#### Números vizinhos ####\n');
+  const entradaNumero = Number(prompt("Digite um número: "));
+  console.log(`Os números vizinhos de ${entradaNumero} são ${entradaNumero - 1} e ${entradaNumero + 1}`);
+};  
+
